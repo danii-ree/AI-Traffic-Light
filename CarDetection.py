8b388c568c556d75e3d2ffa77e7a1ccd7341a936
danii-ree <142275709+danii-ree@users.noreply.github.com>
2023-09-17 14:19:22 -0400

    Added the AI components for the simulator
    These are all of the necessary requirements needed for the AI traffic light simulator to work

---
+++ CarDetection.py
@@ -0,0 +1,25 @@
+"""
+CAR DETECTION
+"""
+import cv2 
+
+cars_cascade = cv2.CascadeClassifier('cars.xml')
+
+cap = cv2.VideoCapture('cars.mp4')
+
+while True:
+    _, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    cars = cars_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for(x,y,w,h) in cars:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-THIS IS THE TRAFFIC AI CODE. 
+THE TRAFFIC AI CODE.
+****************************
+This is simply a simulator, but it could definitely be used in real life. We can implement this either by using an 
+Arduino or a traffic light circuit.
 """
 from tkinter import * 
 
@@ -10,20 +13,36 @@ width = 400
 height = 600
 root.geometry(f'{width}x{height}')
 
+red = False
+yellow = False
+green = False
+
+lightColor = "black"
+
+# Creating a  canvas for 
 myCanvas = Canvas(root)
 myCanvas.pack()
 
-def CreateCircle(x, y, r, canvas):
+def redButton(lightSwitch):
+    print("Red Button is working")
+    red = True
+    if red == True:
+        lightSwitch = "red"
+
+# function for creating a circle
+def CreateCircle(x, y, r, canvas, fillColor = lightColor):
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
-    return canvas.create_oval(x0, y0, x1, y1)
+    return canvas.create_oval(x0, y0, x1, y1, fill=fillColor)
 
-CreateCircle(150, 220, 20, myCanvas)
+CreateCircle(190, 120, 20, myCanvas)
+CreateCircle(190, 160, 20, myCanvas)
+CreateCircle(190, 200, 20, myCanvas)
 
 # setting up the buttons - Red, Yellow, Green is needed to light up traffic lights
-redButton = Button(root, text="Red")
+redButton = Button(root, text="Red", command=redButton(lightColor))
 redButton.pack(side="left", padx=20)
 
 yellowButton = Button(root, text="Yellow")
