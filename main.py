40853cee064f5f4f8a8e44a3f69fb26680e90442
danii-ree <142275709+danii-ree@users.noreply.github.com>
2023-09-20 19:31:38 -0400

    Removed The two detection files. Replaced with Detection.py instead

--- main.py
+++ main.py
@@ -5,78 +5,101 @@ This is simply a simulator, but it could definitely be used in real life. We can
 Arduino or a traffic light circuit.
 """
 from tkinter import * 
+import customtkinter as ctk
+from PIL import ImageTk, Image
+from Detection import *
+
+ctk.set_appearance_mode("#f0f0f0")
+ctk.set_default_color_theme("blue")
 
 # intiallizing all the necessary components (root, width, height, title, etc.)
-root = Tk()
+root = ctk.CTk()
 root.title("Traffic Light Simulator")
-width = 400
+width = 600
 height = 600
 root.geometry(f'{width}x{height}')
 
+global red, yellow, green
 red = False
 yellow = False
 green = False
 
-# Creating a  canvas for 
-myCanvas = Canvas(root)
+# Creating a  canvas for traffic lights
+myCanvas = Canvas(root, bg="#f0f0f0", borderwidth=0)
 myCanvas.pack()
 
-# function for creating a circle
+# Images of the traffic light
+tflight_bg = Image.open('./Traffic Light Images/tfLight_bg.png')
+light_off1 = Image.open('./Traffic Light Images/light_off1.png')
+light_off2 = Image.open('./Traffic Light Images/light_off2.png')
+light_off3 = Image.open('./Traffic Light Images/light_off3.png')
+Red_light = Image.open('./Traffic Light Images/Red.png')
+Yellow_light = Image.open('./Traffic Light Images/Yellow.png')
+Green_light = Image.open('./Traffic Light Images/Green.png')
+
+tflight_bg = tflight_bg.resize((100, 150))
+light_off1 = light_off1.resize((100, 150))
+light_off2 = light_off2.resize((100, 150))
+light_off3 = light_off3.resize((100, 150))
+Red_light = Red_light.resize((100, 150))
+Yellow_light = Yellow_light.resize((100, 150))
+Green_light = Green_light.resize((100, 150))
+
+test1 = ImageTk.PhotoImage(tflight_bg)
+test2 = ImageTk.PhotoImage(light_off1)
+test3 = ImageTk.PhotoImage(light_off2)
+test4 = ImageTk.PhotoImage(light_off3)
+test5 = ImageTk.PhotoImage(Red_light)
+test6 = ImageTk.PhotoImage(Yellow_light)
+test7 = ImageTk.PhotoImage(Green_light)
+
+myCanvas.create_image(150, 150, image=test1)
+
 def CreateCircle(x, y, r, canvas, fillColor = "black"):
     x0 = x - r
     y0 = y - r
     x1 = x + r
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1, fill=fillColor)
+
+# functions for turning the lights on or off
 def lightsOff():
-    CreateCircle(190, 120, 20, myCanvas, "black")
-    CreateCircle(190, 160, 20, myCanvas, "black")
-    CreateCircle(190, 200, 20, myCanvas, "black")
-# STILL WORKING ON IT
+    myCanvas.create_image(150, 150, image=test2)
+    myCanvas.create_image(150, 150, image=test3)
+    myCanvas.create_image(150, 150, image=test4)
+    
 def redLight():
     global red, yellow, green
-    red = True
-    yellow = False 
-    green = False
 
-    if red == True: 
-        CreateCircle(190, 120, 20, myCanvas, "red")
-        red = False
-    elif red == False: 
-        myCanvas.delete('all')
+    if pedestrian_count == 0 and car_count > 0 and yellow == False: 
+        if red:
+            myCanvas.create_image(150, 150, image=test5)
+            red = False
+        else:
+            myCanvas.create_image(150, 150, image=test2)
+            red = True
+    
 def yellowLight():
     global red, yellow, green
-    red = False
-    yellow = True 
-    green = False
 
-    print(red)
-    if yellow == True: 
-        CreateCircle(190, 160, 20, myCanvas, "yellow")
-    else: 
-        CreateCircle(190, 160, 20, myCanvas, "black")
+    if pedestrian_count == 0 and car_count > 0 and red == False: 
+        if yellow: 
+            myCanvas.create_image(150, 150, image=test6)
+            yellow = False
+        else: 
+            myCanvas.create_image(150, 150, image=test3)
+            yellow = True
 def greenLight():
     global red, yellow, green
-    red = False
-    yellow = False 
-    green = True
+    if green: 
+        myCanvas.create_image(150, 150, image=test7)
+        green = False
+    else:
+        myCanvas.create_image(150, 150, image=test4)
+        green = True
 
-    if green == True: 
-        CreateCircle(190, 200, 20, myCanvas, "green")
-    else: 
-        CreateCircle(190, 200, 20, myCanvas, "black")
+start_video_thread()
 
 lightsOff()
-
-# setting up the buttons - Red, Yellow, Green is needed to light up traffic lights
-redButton = Button(root, text="Red", command=redLight)
-redButton.pack(side="left", padx=20)
-
-yellowButton = Button(root, text="Yellow", command=yellowLight)
-yellowButton.pack(side="left", padx=80)
-
-greenButton = Button(root, text="Green", command=greenLight)
-greenButton.pack(side="right", padx=30)
-
 
 root.mainloop()
